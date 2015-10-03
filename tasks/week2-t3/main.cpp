e7eed233c90094bd2eca864ddc7e46f788024d06
Vladimir Petrigo <taenaru@gmail.com>
2015-10-03 12:09:45 +0300

    Solution for week 2, task 3

---
+++ tasks/week2-t3/main.cpp
@@ -0,0 +1,49 @@
+/*
+Author: vpetrigo
+Task:
+Даны три целых числа. Найдите наибольшее из них (программа должна вывести ровно одно целое число).
+
+Формат входных данных
+Вводятся три числа.
+
+Формат выходных данных
+Выведите ответ на задачу.
+
+Sample Input:
+1
+2
+3
+Sample Output:
+3
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if (a > b) {
+        if (a > c) {
+            cout << a;
+        }
+        else {
+            cout << c;
+        }
+    }
+    else {
+        if (b > c) {
+            cout << b;
+        }
+        else {
+            cout << c;
+        }
+    }
+
+
+    return 0;
+}
