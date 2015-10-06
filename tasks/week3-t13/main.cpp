e54dfb4fa6342ec9e472189e74e78f8a6d3c435b
Vladimir Petrigo <taenaru@gmail.com>
2015-10-06 13:50:08 +0300

    Add another solution for week 3, task 13

--- tasks/week3-t13/main.cpp
+++ tasks/week3-t13/main.cpp
@@ -38,6 +38,7 @@ int main() {
     bool prev_gr = false;
     int cnt = 0;
 
+    // Option 1
     while (cin >> cur && cur != 0) {
         if (prev != -1) {
             if (prev < cur && !prev_gr) {
@@ -58,6 +59,25 @@ int main() {
         }
 
         prev = cur;
+    }
+
+    // Option 2
+
+    int next;
+
+    if (cin >> prev && prev != 0) {
+        if (cin >> cur && cur != 0) {
+            if (cin >> next) {
+                while (next != 0) {
+                    if (prev < cur && cur > next) {
+                        ++cnt;
+                    }
+                    prev = cur;
+                    cur = next;
+                    cin >> next;
+                }
+            }
+        }
     }
 
     cout << cnt;
