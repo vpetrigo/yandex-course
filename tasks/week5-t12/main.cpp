7798ff7e01e33c490076c956ec831f30dc1181fa
Vladimir Petrigo <taenaru@gmail.com>
2015-10-10 00:19:28 +0300

    Solution for week 5, task 13

--- tasks/week5-t12/main.cpp
+++ tasks/week5-t12/main.cpp
@@ -26,36 +26,28 @@ using namespace std;
 int main() {
     int q;
     vector<int> v;
-    vector<int> unique;
 
     cin >> q;
+
+    vector<int> uni(q);
 
     while (cin >> q) {
         v.push_back(q);
     }
 
-    bool u = true; // is element unique
-
     for (int i = 0; i < v.size(); ++i) {
-        swap(v[0], v[i]);
-
-        for (int j = 1; j < v.size() && u; ++j) {
-            if (v[0] == v[j]) {
-                u = false;
+        for (int j = i + 1; j < v.size(); ++j) {
+            if (v[i] == v[j]) {
+                ++uni[i];
+                ++uni[j];
             }
         }
-        
-        swap(v[0], v[i]);
-
-        if (u) {
-            unique.push_back(v[i]);
-        }
-
-        u = true;
     }
 
-    for (int i = 0; i < unique.size(); ++i) {
-        cout << unique[i] << ' ';
+    for (int i = 0; i < uni.size(); ++i) {
+        if (uni[i] == 0) {
+            cout << v[i] << ' ';
+        }
     }
 
     return 0;
---
+++ tasks/week5-t13/main.cpp
@@ -0,0 +1,72 @@
+/*
+Author: vpetrigo
+Task:
+Известно, что на доске 8?8 можно расставить 8 ферзей так, чтобы они не били друг друга. 
+Вам дана расстановка 8 ферзей на доске, определите, есть ли среди них пара бьющих друг друга.
+
+Формат входных данных
+Программа получает на вход восемь пар чисел, каждое число от 1 до 8 - координаты 8 ферзей.
+
+Формат выходных данных
+Если ферзи не бьют друг друга, выведите слово NO, иначе выведите YES.
+
+Sample Input 1:
+1 7
+2 4
+3 2
+4 8
+5 6
+6 1
+7 3
+8 5
+Sample Output 1:
+NO
+
+Sample Input 2:
+1 8
+2 7
+3 6
+4 5
+5 4
+6 3
+7 2
+8 1
+Sample Output 2:
+YES
+
+*/
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    vector<int> v;
+
+    while (cin >> x >> y) {
+        v.push_back(x);
+        v.push_back(y);
+    }
+
+    bool pair = false;
+
+    for (int i = 0; i < v.size() && !pair; i += 2) {
+        for (int j = i + 2; j < v.size() && !pair; j += 2) {
+            if (v[i] == v[j] || v[i + 1] == v[j + 1] || abs(v[i] - v[j]) == abs(v[i + 1] - v[j + 1])) {
+                pair = true;
+            }
+        }
+    }
+
+    if (pair) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+
+    return 0;
+}
