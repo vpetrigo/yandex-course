d94fc9886f2ab11425bc68e94f97a9fb3bf80f55
Vladimir Petrigo <taenaru@gmail.com>
2015-10-16 15:29:12 +0300

    Solution for week 7, task 2

---
+++ tasks/week7-t2/main.cpp
@@ -0,0 +1,39 @@
+/*
+Author: vpetrigo
+Task:
+Даны четыре действительных числа: x1, y1, x2, y2. 
+Напишите функцию distance(x1, y1, x2, y2), вычисляющую расстояние между точкой (x1. y1) и (x2, y2). 
+Считайте четыре действительных числа и выведите результат работы этой функции.
+
+Формат входных данных
+Вводятся четыре действительных числа.
+Формат выходных данных
+Выведите ответ на задачу.
+
+Sample Input:
+0
+0
+1
+1
+Sample Output:
+1.41421
+
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double distance(double x1, double y1, double x2, double y2) {
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+
+int main() {
+    double x1, y1, x2, y2;
+    
+    cin >> x1 >> y1 >> x2 >> y2;
+    cout << distance(x1, y1, x2, y2);
+
+    return 0;
+}
