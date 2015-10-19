ee1b71abf1ec6b17342f12e74ac609641daa4cf6
Vladimir Petrigo <taenaru@gmail.com>
2015-10-19 18:30:18 +0300

    Solution for week 7, task 10

---
+++ tasks/week7-t10/main.cpp
@@ -0,0 +1,59 @@
+/*
+Author: vpetrigo
+Task:
+Возводить в степень можно гораздо быстрее, чем за n умножений! 
+Для этого нужно воспользоваться следующими рекуррентными соотношениями:
+
+a^n = (a^2)^(n/2)  при четном n,
+
+a^n = a ? a^(n?1) при нечетном n.
+
+Реализуйте алгоритм быстрого возведения в степень с помощью рекурсивной функции.
+
+Формат входных данных
+Вводятся действительное число a и целое число n.
+
+Формат выходных данных
+Выведите ответ на задачу.
+Sample Input 1:
+2.0
+1
+Sample Output 1:
+2
+
+Sample Input 2:
+2
+2
+Sample Output 2:
+4
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+double fast_pow(double a, int n) {
+    double res = 1;
+
+    if (n != 0) {
+        if (n % 2 == 0) {
+            res = fast_pow(a * a, n / 2);
+        }
+        else {
+            res = a * fast_pow(a, n - 1);
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    double a;
+    int n;
+
+    cin >> a >> n;
+    cout << fast_pow(a, n);
+
+    return 0;
+}
