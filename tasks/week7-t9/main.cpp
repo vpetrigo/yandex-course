92d6697b7b62035ec03f534e8015ea8c16e05f4a
Vladimir Petrigo <taenaru@gmail.com>
2015-10-19 18:18:37 +0300

    Solution for week 7, task 9

---
+++ tasks/week7-t9/main.cpp
@@ -0,0 +1,54 @@
+/*
+Author: vpetrigo
+Task:
+Дано натуральное число n > 1. Проверьте, является ли оно простым. 
+Программа должна вывести слово YES, если число простое и NO, если число составное.
+Решение оформите в виде функции IsPrime(n), которая возвращает 
+True для простых чисел и False для составных чисел. Количество операций в программе должно быть пропорционально корню из n.
+
+Формат входных данных
+Вводится натуральное число.
+
+Формат выходных данных
+Выведите ответ на задачу.
+Sample Input 1:
+2
+Sample Output 1:
+YES
+
+Sample Input 2:
+4
+Sample Output 2:
+NO
+
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool IsPrime(int n) {
+    int div = 2;
+
+    while (div < sqrt(n) && n % div != 0) {
+        ++div;
+    }
+
+    return (n % div != 0 || n == div);
+}
+
+int main() {
+    int n;
+
+    cin >> n;
+    
+    if (IsPrime(n)) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+
+    return 0;
+}
