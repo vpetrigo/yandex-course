c31e36d325318f26e902fa63fc0af6f70c7c6800
Vladimir Petrigo <taenaru@gmail.com>
2015-10-19 15:48:08 +0300

    Solution for week 7, task 7

---
+++ week7-t7/main.cpp
@@ -0,0 +1,54 @@
+/*
+Author: vpetrigo
+Task:
+Дано действительное положительное число a и целоe число n.
+
+Вычислите an. Решение оформите в виде функции power(a, n).
+
+Формат входных данных
+Вводится действительное положительное число a и целоe число n.
+Формат выходных данных
+Выведите ответ на задачу.
+Sample Input 1:
+2
+1
+Sample Output 1:
+2
+
+Sample Input 2:
+2
+2
+Sample Output 2:
+4
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+double power(double x, int n) {
+    double res = 1;
+
+    if (n < 0) {
+        x = 1 / x;
+        n = -n;
+    }
+    
+    while (--n >= 0) {
+        res *= x;
+    }
+
+    return res;
+}
+
+int main() {
+    double a;
+    int n;
+
+    cin >> a >> n;
+
+    cout << power(a, n) << endl;
+
+    return 0;
+}
